2d280fbf401138b0e3dc13e44ebf32c4ff81c466
Lewen Sun <112133020+lxsun2026@users.noreply.github.com>
2024-06-01 18:51:41 -0400

    Update expiration_date_scraper.py

--- expiration_date_scraper.py
+++ expiration_date_scraper.py
@@ -1,3 +1,6 @@
+#use Selenium to scrape StillTasty.com and create a 
+#table with ingredient names and their shelf lives
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
